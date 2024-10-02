844bbbef7ac705f13ad1311f5dfce279610ae152
my name <tainv1101@gmail.com>
2024-10-02 22:47:48 +0700

    first commit

---
+++ src/config/mongodb.js
@@ -0,0 +1,9 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+//tainv1101
+//Ry6nV7S8ea5RUV0H
+const MONGODB_URI = 'mongodb+srv://tainv1101:Ry6nV7S8ea5RUV0H@trello.fiygk.mongodb.net/?retryWrites=true&w=majority&appName=Trello';
+const DATABASE_NAME = 'trello-db';
